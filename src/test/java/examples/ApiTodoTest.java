368ac352356dc408af33a71a30536e07fd0ea099
brunnalu <brunnalu@gmail.con>
2025-08-04 20:30:11 -0300

    atualização

--- src/test/java/examples/ApiTodoTest.java
+++ src/test/java/examples/ApiTodoTest.java
@@ -1,12 +1,5 @@
-package examples;
-
-import com.intuit.karate.junit5.Karate;
-
-public class ApiTodoTest {
-
-    @Karate.Test
-    public Karate testTodosFeature() {
-        System.out.println("Iniciando execução do Karate...");
-        return Karate.run("classpath:examples/todo/todo.feature");
-    }
-}
+package examples; 
+import com.intuit.karate.junit5.Karate; 
+public class ApiTodoTest { @Karate.Test public Karate testTodosFeature() 
+    { System.out.println("Iniciando execução do Karate..."); 
+    return Karate.run("classpath:examples/todo/todo.feature"); } }
